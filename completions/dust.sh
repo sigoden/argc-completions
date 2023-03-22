bc7815be294db405d5495b574921145100921c9c
sigoden <sigoden@gmail.com>
2023-03-22 23:40:53 +0800

    feat: add dust.sh

---
+++ completions/dust.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Generated with `./generate.sh --spec=clap dust`. DON'T MODIFY IT
+
+# @flag -b --no-percent-bars
+# @flag -c --no-colors
+# @option -d --depth
+# @flag -D --only-dir
+# @option -e --filter
+# @flag -f --filecount
+# @flag -F --only-file
+# @flag -H --si
+# @flag -i --ignore_hidden
+# @flag -L --dereference-links
+# @option -n --number-of-lines <number_of_lines>
+# @flag -p --full-paths
+# @flag -P --no-progress
+# @flag -r --reverse
+# @flag -R --screen-reader
+# @flag -s --apparent-size
+# @flag --skip-total
+# @flag -t --file_types
+# @option -v --invert-filter <invert_filter>
+# @option -w --terminal_width <width>
+# @flag -x --limit-filesystem
+# @option -X --ignore-directory <ignore_directory>
+# @option -z --min-size <min_size>
+# @arg dir*
+
+eval "$(argc "$0" "$@")"
