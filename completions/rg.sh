35c61c3bdf27c2870bbcccad5511ceea5ea58b2a
sigoden <sigoden@gmail.com>
2023-03-23 09:27:58 +0800

    feat: add rg.sh

---
+++ completions/rg.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+# Generated with `./generate.sh -a=-h --spec=clap rg`. DON'T MODIFY IT
+
+# @option -A --after-context <NUM>
+# @flag --auto-hybrid-regex
+# @option -B --before-context <NUM>
+# @flag --binary
+# @flag --block-buffered
+# @flag -b --byte-offset
+# @flag -s --case-sensitive
+# @option --color <WHEN>
+# @option --colors* <COLOR_SPEC>
+# @flag --column
+# @option -C --context <NUM>
+# @option --context-separator <SEPARATOR>
+# @flag -c --count
+# @flag --count-matches
+# @flag --crlf
+# @flag --debug
+# @option --dfa-size-limit <NUM+SUFFIX?>
+# @option -E --encoding
+# @option --engine
+# @option --field-context-separator <SEPARATOR>
+# @option --field-match-separator <SEPARATOR>
+# @option -f --file* <PATTERNFILE>
+# @flag --files
+# @flag -l --files-with-matches
+# @flag --files-without-match
+# @flag -F --fixed-strings
+# @flag -L --follow
+# @option -g --glob*
+# @flag --glob-case-insensitive
+# @flag --heading
+# @flag --hidden
+# @option --iglob* <GLOB>
+# @flag -i --ignore-case
+# @option --ignore-file* <PATH>
+# @flag --ignore-file-case-insensitive
+# @flag --include-zero
+# @flag -v --invert-match
+# @flag --json
+# @flag --line-buffered
+# @flag -n --line-number
+# @flag -x --line-regexp
+# @option -M --max-columns <NUM>
+# @flag --max-columns-preview
+# @option -m --max-count <NUM>
+# @option --max-depth <NUM>
+# @option --max-filesize <NUM+SUFFIX?>
+# @flag --mmap
+# @flag -U --multiline
+# @flag --multiline-dotall
+# @flag --no-config
+# @flag -I --no-filename
+# @flag --no-heading
+# @flag --no-ignore
+# @flag --no-ignore-dot
+# @flag --no-ignore-exclude
+# @flag --no-ignore-files
+# @flag --no-ignore-global
+# @flag --no-ignore-messages
+# @flag --no-ignore-parent
+# @flag --no-ignore-vcs
+# @flag -N --no-line-number
+# @flag --no-messages
+# @flag --no-mmap
+# @flag --no-pcre2-unicode
+# @flag --no-require-git
+# @flag --no-unicode
+# @flag --null
+# @flag --null-data
+# @flag --one-file-system
+# @flag -o --only-matching
+# @flag --passthru
+# @option --path-separator <SEPARATOR>
+# @flag -P --pcre2
+# @flag --pcre2-version
+# @option --pre <FILE>
+# @option --pre-glob* <GLOB>
+# @flag -p --pretty
+# @flag -q --quiet
+# @option --regex-size-limit <NUM+SUFFIX?>
+# @option -e --regexp* <PATTERN>
+# @option -r --replace <REPLACEMENT_TEXT>
+# @flag -z --search-zip
+# @flag -S --smart-case
+# @option --sort <SORTBY>
+# @option --sortr <SORTBY>
+# @flag --stats
+# @flag -a --text
+# @option -j --threads <NUM>
+# @flag --trim
+# @option -t --type*
+# @option --type-add* <TYPE_SPEC>
+# @option --type-clear* <TYPE>
+# @flag --type-list
+# @option -T --type-not* <TYPE>
+# @flag -u --unrestricted
+# @flag --vimgrep
+# @flag -H --with-filename
+# @flag -w --word-regexp
+# @arg pattern
+# @arg path*
+
+eval "$(argc "$0" "$@")"
