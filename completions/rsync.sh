3377396d1ad23c45a11fc661e12ccefeb3779191
sigoden <sigoden@gmail.com>
2023-03-28 08:29:40 +0800

    feat: add rsync

---
+++ completions/rsync.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+# Generated with `./generate.sh rsync`. DON'T MODIFY IT
+
+# @flag -v --verbose
+# @option --info <FLAGS>
+# @option --debug <FLAGS>
+# @option --stderr[e|a|c]
+# @flag -q --quiet
+# @flag --no-motd
+# @flag -c --checksum
+# @flag -a --archive
+# @flag --no-OPTION
+# @flag -r --recursive
+# @flag -R --relative
+# @flag --no-implied-dirs
+# @flag -b --backup
+# @option --backup-dir <DIR>
+# @option --suffix
+# @flag -u --update
+# @flag --inplace
+# @flag --append
+# @flag --append-verify
+# @flag -d --dirs
+# @flag --mkpath
+# @flag -l --links
+# @flag -L --copy-links
+# @flag --copy-unsafe-links
+# @flag --safe-links
+# @flag --munge-links
+# @flag -k --copy-dirlinks
+# @flag -K --keep-dirlinks
+# @flag -H --hard-links
+# @flag -p --perms
+# @flag -E --executability
+# @option --chmod
+# @flag -A --acls
+# @flag -X --xattrs
+# @flag -o --owner
+# @flag -g --group
+# @flag --devices
+# @flag --copy-devices
+# @flag --specials
+# @flag -D
+# @flag -t --times
+# @flag -U --atimes
+# @flag --open-noatime
+# @flag -N --crtimes
+# @flag -O --omit-dir-times
+# @flag -J --omit-link-times
+# @flag --super
+# @flag --fake-super
+# @flag -S --sparse
+# @flag --preallocate
+# @flag --write-devices
+# @flag -n --dry-run
+# @flag -W --whole-file
+# @option --checksum-choice <STR>
+# @flag -x --one-file-system
+# @option -B --block-size <SIZE>
+# @option -e --rsh <COMMAND>
+# @option --rsync-path <PROGRAM>
+# @flag --existing
+# @flag --ignore-existing
+# @flag --remove-source-files
+# @flag --del
+# @flag --delete
+# @flag --delete-before
+# @flag --delete-during
+# @flag --delete-delay
+# @flag --delete-after
+# @flag --delete-excluded
+# @flag --ignore-missing-args
+# @flag --delete-missing-args
+# @flag --ignore-errors
+# @flag --force
+# @option --max-delete <NUM>
+# @option --max-size <SIZE>
+# @option --min-size <SIZE>
+# @option --max-alloc <SIZE>
+# @flag --partial
+# @option --partial-dir <DIR>
+# @flag --delay-updates
+# @flag -m --prune-empty-dirs
+# @flag --numeric-ids
+# @option --usermap <STRING>
+# @option --groupmap <STRING>
+# @option --chown <USER:GROUP>
+# @option --timeout <SECONDS>
+# @option --contimeout <SECONDS>
+# @flag -I --ignore-times
+# @flag --size-only
+# @option -@ --modify-window <NUM>
+# @option -T --temp-dir <DIR>
+# @flag -y --fuzzy
+# @option --compare-dest <DIR>
+# @option --copy-dest <DIR>
+# @option --link-dest <DIR>
+# @flag -z --compress
+# @option --compress-choice <STR>
+# @option --compress-level <NUM>
+# @option --skip-compress <LIST>
+# @flag -C --cvs-exclude
+# @option -f --filter <RULE>
+# @flag -F
+# @option --exclude <PATTERN>
+# @option --exclude-from <FILE>
+# @option --include <PATTERN>
+# @option --include-from <FILE>
+# @option --files-from <FILE>
+# @flag -0 --from0
+# @flag -s --protect-args
+# @option --copy-as <USER:GROUP>
+# @option --address
+# @option --port
+# @option --sockopts <OPTIONS>
+# @flag --blocking-io
+# @option --outbuf[N|L|B]
+# @flag --stats
+# @flag -8 --8-bit-output
+# @flag -h --human-readable
+# @flag --progress
+# @flag -P
+# @flag -i --itemize-changes
+# @option -M --remote-option <OPT>
+# @option --out-format <FORMAT>
+# @option --log-file <FILE>
+# @option --log-file-format <FMT>
+# @option --password-file <FILE>
+# @option --early-input <FILE>
+# @flag --list-only
+# @option --bwlimit <RATE>
+# @option --stop-after <MINS>
+# @option --stop-at <y-m-dTh:m>
+# @option --write-batch <FILE>
+# @option --only-write-batch <FILE>
+# @option --read-batch <FILE>
+# @option --protocol <NUM>
+# @option --iconv <CONVERT_SPEC>
+# @option --checksum-seed <NUM>
+# @flag -4 --ipv4
+# @flag -6 --ipv6
+# @arg path*
+
+eval "$(argc "$0" "$@")"
