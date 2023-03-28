302bb3d20711dd462f650a0a819dde9731385564
sigoden <sigoden@gmail.com>
2023-03-28 08:09:58 +0800

    feat: add wget

---
+++ completions/wget.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# Generated with `./generate.sh wget`. DON'T MODIFY IT
+
+# @flag -b --background
+# @option -e --execute <COMMAND>
+# @option -o --output-file <FILE>
+# @option -a --append-output <FILE>
+# @flag -d --debug
+# @flag -q --quiet
+# @flag -v --verbose
+# @flag --no-verbose
+# @option --report-speed <TYPE>
+# @option -i --input-file <FILE>
+# @flag -F --force-html
+# @option -B --base <URL>
+# @option --config <FILE>
+# @flag --no-config
+# @option --rejected-log <FILE>
+# @option -t --tries <NUMBER>
+# @flag --retry-connrefused
+# @option --retry-on-http-error <ERRORS>
+# @option -O --output-document <FILE>
+# @flag --no-clobber
+# @flag --no-netrc
+# @flag -c --continue
+# @option --start-pos <OFFSET>
+# @option --progress <TYPE>
+# @flag --show-progress
+# @flag -N --timestamping
+# @flag --no-if-modified-since
+# @flag --no-use-server-timestamps
+# @flag -S --server-response
+# @flag --spider
+# @option -T --timeout <SECONDS>
+# @option --dns-timeout <SECS>
+# @option --connect-timeout <SECS>
+# @option --read-timeout <SECS>
+# @option -w --wait <SECONDS>
+# @option --waitretry <SECONDS>
+# @flag --random-wait
+# @flag --no-proxy
+# @option -Q --quota <NUMBER>
+# @option --bind-address <ADDRESS>
+# @option --limit-rate <RATE>
+# @flag --no-dns-cache
+# @option --restrict-file-names <OS>
+# @flag --ignore-case
+# @flag -4 --inet4-only
+# @flag -6 --inet6-only
+# @option --prefer-family <FAMILY>
+# @option --user
+# @option --password <PASS>
+# @flag --ask-password
+# @option --use-askpass <COMMAND>
+# @flag --no-iri
+# @option --local-encoding <ENC>
+# @option --remote-encoding <ENC>
+# @flag --unlink
+# @flag --xattr
+# @flag --no-directories
+# @flag -x --force-directories
+# @flag --no-host-directories
+# @flag --protocol-directories
+# @option -P --directory-prefix <PREFIX>
+# @option --cut-dirs <NUMBER>
+# @option --http-user <USER>
+# @option --http-password <PASS>
+# @flag --no-cache
+# @flag --default-page
+# @arg url*
+
+eval "$(argc "$0" "$@")"
