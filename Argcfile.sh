94daebfba4118ca14ae52d6c660a6e918fe06b8d
sigoden <sigoden@gmail.com>
2023-05-20 13:59:36 +0800

    feat(Argcfile): add run-choice-fn

--- Argcfile.sh
+++ Argcfile.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # @cmd
-# @arg cmd[?`_choice_command`]
+# @arg cmd[?`_choice_completion`]
 generate() {
     if [ $# -eq 0 ]; then
         while read -r name; do
             echo Generate $name
             argc generate $name
-        done < <(_choice_command)
+        done < <(_choice_completion)
     else
         ./generate.sh -o completions $1
     fi
@@ -19,24 +19,39 @@ test() {
 }
 
 # @cmd
+# @arg cmd![`_choice_completion_or_src`]
+# @arg fn![`_choice_fn_name`]
+# @arg line
+run-choice-fn() {
+    cmd_script="completions/$argc_cmd.sh"
+    if [[ -n "$argc_line" ]] && [[ -f "$cmd_script" ]]; then
+        argc_line=${argc_line#"$argc_cmd"} 
+        bash "$cmd_script" $argc_fn "$argc_line"
+    else
+        . "src/$argc_cmd.sh"
+        $argc_fn
+    fi
+}
+
+# @cmd
 # @option -k --kind[=table|help|script]
-# @arg cmd![?`_choice_command`]
+# @arg cmd![?`_choice_completion`]
 # @arg args*
-print() {
+check() {
     if [[ "$argc_kind" == "help" ]]; then
-        print-help $@
+        check-help $@
     elif [[ "$argc_kind" == "table" ]]; then
-        print-table $@
+        check-table $@
     elif [[ "$argc_kind" == "script" ]]; then
-        print-script $@
+        check-script $@
     fi
 }
 
 # @cmd
 # @flag -P --no-patch
-# @arg cmd![?`_choice_command`]
+# @arg cmd![?`_choice_completion`]
 # @arg args*
-print-help() {
+check-help() {
       ( set -o posix ; set ) | grep argc_
     _source_src $1
     if _test_patch_fn help; then
@@ -48,11 +63,11 @@ print-help() {
 
 # @cmd
 # @flag -P --no-patch
-# @arg cmd![?`_choice_command`]
+# @arg cmd![?`_choice_completion`]
 # @arg args*
-print-table() {
+check-table() {
     _source_src $1
-    help_text=$(print-help $@ | awk -f scripts/lexer.awk)
+    help_text=$(check-help $@ | awk -f scripts/lexer.awk)
     if _test_patch_fn table; then
         echo "$help_text" | _patch_table $@
     else
@@ -62,11 +77,11 @@ print-table() {
 
 # @cmd
 # @flag -P --no-patch
-# @arg cmd![?`_choice_command`]
+# @arg cmd![?`_choice_completion`]
 # @arg args*
-print-script() {
+check-script() {
     _source_src $1
-    table_text=$(print-table $@ | awk -f scripts/parser.awk)
+    table_text=$(check-table $@ | awk -f scripts/parser.awk)
     if _test_patch_fn script; then
         echo "$table_text" | _patch_script $@
     else
@@ -74,11 +89,23 @@ print-script() {
     fi
 }
 
-_choice_command() {
-    for f in completions/*; do
-        name=$(basename $f)
-        echo "${name%.*}"
-    done
+_choice_completion() {
+    ls -1 completions | xargs -I% basename % .sh
+}
+
+_choice_src() {
+    ls -1 src | xargs -I% basename % .sh
+}
+
+_choice_completion_or_src() {
+    { _choice_completion; _choice_src; } | sort | uniq
+}
+
+_choice_fn_name() {
+    cmd_script="src/$argc_cmd.sh" 
+    if [[ -f "$cmd_script" ]]; then
+        cat "$cmd_script" | grep ^_choice | sed 's/\(_choice\w\+\).*/\1/'
+    fi
 }
 
 _source_src() {
@@ -98,7 +125,7 @@ _source_src() {
 
 _test_patch_fn() {
     local target=$1
-    if [[ "$argc_no_patch" -eq 1 ]] && [[ "$argc__fn" == "print-$target" ]]; then
+    if [[ "$argc_no_patch" -eq 1 ]] && [[ "$argc__fn" == "check-$target" ]]; then
         return 1
     fi
     if [[ $(type -t _patch_$target) != "function" ]] ; then
