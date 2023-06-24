33c9522ea6b1681c6f917f4a60c797d47059fd3c
sigoden <sigoden@gmail.com>
2023-06-25 06:46:09 +0800

    chore: following argc utils upgrade

--- completions/pnpm.sh
+++ completions/pnpm.sh
@@ -807,7 +807,7 @@ _helper_pkg_json_path() {
 
 _argc_util_path_to_unix() {
     local target="$1"
-    if [[ -z "$target" ]]; then
+    if [[ -z "$target" ]] && [[ $# -eq 0 ]]; then
         target="$(cat)"
     fi
     if [[ "$OS" == "Windows_NT" ]]; then
