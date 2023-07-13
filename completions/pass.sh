0cfacec838903384464dd57c3c3fa62e9f59b943
sigoden <sigoden@gmail.com>
2023-07-13 20:36:08 +0800

    feat: add pass

---
+++ completions/pass.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env bash
+# Automatic generated, DON'T MODIFY IT.
+
+
+
+# {{ pass init
+# @cmd Initialize new password storage and use gpg-id for encryption.
+# @option -p --path <subdir>    Secific sub folder of the password store.
+# @arg gpg-id+[`_choice_gpg_id`]
+init() {
+    :;
+}
+# }} pass init
+
+# {{ pass ls
+# @cmd List passwords.
+# @arg subdir[`_choice_pass_dir`]
+ls() {
+    :;
+}
+# }} pass ls
+
+# {{ pass find
+# @cmd List passwords that match pass-names.
+# @arg pass-string+
+find() {
+    :;
+}
+# }} pass find
+
+# {{ pass show
+# @cmd Show existing password and optionally put it on the clipboard.
+# @option -c --clip <line-number>      Do not print the password but instead copy the first(or otherwise specified) line to the clipboard
+# @option -q --qrcode <line-number>    Do not print the password but instead display a QR code
+# @arg pass-name![`_choice_pass_name`]
+show() {
+    :;
+}
+# }} pass show
+
+# {{ pass grep
+# @cmd Search for password files containing search-string when decrypted.
+# @arg search-string!
+grep() {
+    :;
+}
+# }} pass grep
+
+# {{ pass insert
+# @cmd Insert new password.
+# @flag -e --echo         Echo the password back to the console
+# @flag -m --multiline    Lines will be read until EOF or Ctrl+D is reached
+# @flag -f --force        Forcely overwriting an existing password
+# @arg pass-name![`_choice_pass_name`]
+insert() {
+    :;
+}
+# }} pass insert
+
+# {{ pass edit
+# @cmd Insert a new password or edit an existing password using vim.
+# @arg pass-name![`_choice_pass_name`]
+edit() {
+    :;
+}
+# }} pass edit
+
+# {{ pass generate
+# @cmd Generate a new password of pass-length (or 25 if unspecified) with optionally no symbols.
+# @flag -n --no-symbols    Do not use any non-alphanumeric characters in the generated password.
+# @flag -c --clip          Do not print the password but instead copy it to the clipboard
+# @flag -i --in-place      Do not interactively prompt
+# @flag -f --force         Forcely overwriting an existing password
+# @arg pass-name![`_choice_pass_name`]
+# @arg pass-length
+generate() {
+    :;
+}
+# }} pass generate
+
+# {{ pass rm
+# @cmd Remove existing password or directory, optionally forcefully.
+# @flag -r --recursive    Delete pass-name recursively.
+# @flag -f --force        Do not interactively prompt before removal.
+# @arg pass-name![`_choice_pass_name`]
+rm() {
+    :;
+}
+# }} pass rm
+
+# {{ pass mv
+# @cmd Renames or moves old-path to new-path, optionally forcefully, selectively reencrypting.
+# @flag -f --force    Silently overwrite new-path if it exists
+# @arg old-path![`_choice_pass_name`]
+# @arg new-path![`_choice_pass_name`]
+mv() {
+    :;
+}
+# }} pass mv
+
+# {{ pass cp
+# @cmd Copies old-path to new-path, optionally forcefully, selectively reencrypting.
+# @flag -f --force    Silently overwrite new-path if it exists
+# @arg old-path![`_choice_pass_name`]
+# @arg new-path![`_choice_pass_name`]
+cp() {
+    :;
+}
+# }} pass cp
+
+# {{ pass git
+# @cmd If the password store is a git repository, execute a git command specified by git-command-args.
+# @arg args~[`_choice_git`]
+git() {
+    :;
+}
+# }} pass git
+
+# {{ pass version
+# @cmd Show version information.
+version() {
+    :;
+}
+# }} pass version
+
+. "$ARGC_COMPLETIONS_ROOT/utils/_argc_utils.sh"
+
+_choice_gpg_id() {
+    gpg --list-keys --with-colons | grep '^pub\|^uid' | grep -A1 'pub' | awk -F: '/^pub:|^uid:/ {print $5 $10}'
+}
+
+_choice_pass_dir() {
+    root_dir="$(_helper_root_dir)"
+    command ls -a1dp "$root_dir$ARGC_FILTER"* | grep /$ | sed "s|$root_dir||" | _argc_util_comp_parts /
+}
+
+_choice_pass_name() {
+    root_dir="$(_helper_root_dir)"
+    command ls -a1dp "$root_dir$ARGC_FILTER"* | sed -e "s|$root_dir||" -e 's/\.gpg$//' | _argc_util_comp_parts /
+}
+
+_choice_git() {
+    root_dir="$(_helper_root_dir)"
+    (cd "$root_dir" && _argc_util_comp_subcommand 0 git)
+}
+
+_helper_root_dir() {
+    if [[ -d "$PASSWORD_STORE_DIR" ]]; then
+        if [[ "$PASSWORD_STORE_DIR" =~ "/"$ ]]; then
+            echo "$PASSWORD_STORE_DIR"
+        else
+            echo "$PASSWORD_STORE_DIR/"
+        fi
+    elif [[ -d "$HOME/.password-store" ]]; then
+        echo "$HOME/.password-store/"
+    fi
+}
+
+command eval "$(argc --argc-eval "$0" "$@")"
---
+++ src/pass.sh
@@ -0,0 +1,83 @@
+_patch_help() {
+    if [[ "$*" == "pass" ]]; then
+    $@ --help | sed \
+        -e 's/^Usage:/Commands:/' \
+        -e 's/^    pass \(\S\+\).*$/    \1/' \
+        -e 's/\[ls\]/ls/' \
+        -e 's/\[show\]/show/'
+    else
+        cat <<-'EOF' | _patch_help_select_subcmd $@
+pass init <gpg-id>...
+    -p, --path <subdir>               Secific sub folder of the password store.
+pass ls [subdir]
+pass find <pass-string>...
+pass show <pass-name>
+    -c, --clip <line-number>          Do not print the password but instead copy the first(or otherwise specified) line to the clipboard 
+    -q, --qrcode <line-number>        Do not print the password but instead display a QR code
+pass grep <search-string>
+pass insert <pass-name>
+    -e, --echo                        Echo the password back to the console
+    -m, --multiline                   Lines will be read until EOF or Ctrl+D is reached
+    -f, --force                       Forcely overwriting an existing password
+pass edit <pass-name>
+pass generate <pass-name> [pass-length]
+    -n, --no-symbols                  Do not use any non-alphanumeric characters in the generated password.
+    -c, --clip                        Do not print the password but instead copy it to the clipboard
+    -i, --in-place                    Do not interactively prompt
+    -f, --force                       Forcely overwriting an existing password
+pass rm <pass-name>
+    -r, --recursive                   Delete pass-name recursively.
+    -f, --force                       Do not interactively prompt before removal.
+pass mv <old-path> <new-path>
+    -f, --force                       Silently overwrite new-path if it exists
+pass cp <old-path> <new-path>
+    -f, --force                       Silently overwrite new-path if it exists
+pass git <args>...
+EOF
+    fi
+}
+
+_patch_table() { 
+    if [[ "$*" == "pass init" ]]; then
+        _patch_table_edit_arguments 'gpg-id;[`_choice_gpg_id`]'
+    elif [[ "$*" == "pass ls" ]]; then
+        _patch_table_edit_arguments 'subdir;[`_choice_pass_dir`]'
+    elif [[ "$*" == "pass mv" ]] || [[ "$*" == "pass cp" ]]; then
+        _patch_table_edit_arguments 'old-path;[`_choice_pass_name`]' 'new-path;[`_choice_pass_name`]'
+    elif [[ "$*" == "pass git" ]]; then
+        _patch_table_edit_arguments 'args;~[`_choice_git`]'
+    else
+        _patch_table_edit_arguments 'pass-name;[`_choice_pass_name`]'
+    fi
+}
+
+_choice_gpg_id() {
+    gpg --list-keys --with-colons | grep '^pub\|^uid' | grep -A1 'pub' | awk -F: '/^pub:|^uid:/ {print $5 $10}'
+}
+
+_choice_pass_dir() {
+    root_dir="$(_helper_root_dir)"
+    command ls -a1dp "$root_dir$ARGC_FILTER"* | grep /$ | sed "s|$root_dir||" | _argc_util_comp_parts /
+}
+
+_choice_pass_name() {
+    root_dir="$(_helper_root_dir)"
+    command ls -a1dp "$root_dir$ARGC_FILTER"* | sed -e "s|$root_dir||" -e 's/\.gpg$//' | _argc_util_comp_parts /
+}
+
+_choice_git() {
+    root_dir="$(_helper_root_dir)"
+    (cd "$root_dir" && _argc_util_comp_subcommand 0 git)
+}
+
+_helper_root_dir() {
+    if [[ -d "$PASSWORD_STORE_DIR" ]]; then
+        if [[ "$PASSWORD_STORE_DIR" =~ "/"$ ]]; then
+            echo "$PASSWORD_STORE_DIR"
+        else
+            echo "$PASSWORD_STORE_DIR/"
+        fi
+    elif [[ -d "$HOME/.password-store" ]]; then
+        echo "$HOME/.password-store/"
+    fi
+}
