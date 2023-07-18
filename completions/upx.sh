ded8e2e1f67c46ab21b678c1f0246b5bd3f7713b
sigoden <sigoden@gmail.com>
2023-07-19 07:53:52 +0800

    feat: add upx

---
+++ completions/upx.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+# Automatic generated, DON'T MODIFY IT.
+
+# @flag -1                                        compress faster
+# @flag -9                                        compress better
+# @flag --best                                    compress best (can be slow for big files)
+# @flag -d                                        decompress
+# @flag -l                                        list compressed file
+# @flag -t                                        test compressed file
+# @flag -V                                        display version number
+# @flag -h                                        give this help
+# @flag -L                                        display software license
+# @flag -q                                        be quiet
+# @flag -v                                        be verbose
+# @option -o <FILE>                               write output to 'FILE'
+# @flag -f                                        force compression of suspicious files
+# @flag --no-color                                change look
+# @flag --mono                                    change look
+# @flag --color                                   change look
+# @flag --no-progress                             change look
+# @flag --brute                                   try all available compression methods & filters [slow]
+# @flag --ultra-brute                             try even more compression variants [very slow]
+# @flag -k --backup                               keep backup files
+# @flag --no-backup                               no backup files [default]
+# @option --overlay[`__choice_overlay`] <copy>    copy any extra data attached to the file [default]
+# @flag --coff                                    produce COFF output [default: EXE]
+# @flag --8086                                    make compressed com work on any 8086
+# @flag --no-reloc                                put no relocations in to the exe header
+# @flag --8-bit                                   uses 8 bit size compression [default: 32 bit]
+# @flag --8mib-ram                                8 megabyte memory limit [default: 2 MiB]
+# @flag --boot-only                               disables client/host transfer compatibility
+# @flag --no-align                                don't align to 2048 bytes [enables: --console-run]
+# @flag --le                                      produce LE output [default: EXE]
+# @option --compress-exports[`_choice_compress_exports`] <0>  do not compress the export section
+# @option --compress-icons[`_choice_compress_icons`] <0>  do not compress any icons
+# @option --compress-resources <0>                do not compress any resources at all
+# @option --keep-resource <list>                  do not compress resources specified by list
+# @option --strip-relocs[`_choice_strip_relocs`] <0>  do not strip relocations
+# @flag --preserve-build-id                       copy .gnu.note.build-id to compressed output
+# @arg file*
+
+__choice_overlay() {
+    cat <<-'EOF'
+copy	copy any extra data attached to the file [default]
+strip	strip any extra data attached to the file
+skip	don't compress a file with an overlay
+EOF
+}
+
+_choice_compress_exports() {
+    cat <<-'EOF'
+0	do not compress the export section
+1	compress the export section [default]
+EOF
+}
+
+_choice_compress_icons() {
+    cat <<-'EOF'
+0	do not compress any icons
+1	compress all but the first icon
+2	compress all but the first icon directory [default]
+3	compress all icons
+EOF
+}
+
+_choice_strip_relocs() {
+    cat <<-'EOF'
+0	do not strip relocations
+1	strip relocations [default]
+EOF
+}
+
+command eval "$(argc --argc-eval "$0" "$@")"
---
+++ src/upx.sh
@@ -0,0 +1,53 @@
+_patch_help() { 
+    $@ --help | \
+    _patch_help_split_two_columns | \
+    sed 's/-oFILE/-o FILE /'
+}
+
+_patch_table() { 
+    _patch_table_dedup_options \
+        ';;' \
+        '--overlay' \
+        '--8086' \
+        '--compress-exports' \
+        '--compress-icons' \
+        '--strip-relocs' \
+    | \
+    _patch_table_edit_options \
+        '--overlay;[`__choice_overlay`]' \
+        '--compress-exports;[`_choice_compress_exports`]' \
+        '--compress-icons;[`_choice_compress_icons`]' \
+        '--strip-relocs;[`_choice_strip_relocs`]' \
+
+}
+
+__choice_overlay() {
+    cat <<-'EOF'
+copy	copy any extra data attached to the file [default]
+strip	strip any extra data attached to the file
+skip	don't compress a file with an overlay
+EOF
+}
+
+_choice_compress_exports() {
+    cat <<-'EOF'
+0	do not compress the export section
+1	compress the export section [default]
+EOF
+}
+
+_choice_compress_icons() {
+    cat <<-'EOF'
+0	do not compress any icons
+1	compress all but the first icon
+2	compress all but the first icon directory [default]
+3	compress all icons
+EOF
+}
+
+_choice_strip_relocs() {
+    cat <<-'EOF'
+0	do not strip relocations
+1	strip relocations [default]
+EOF
+}
