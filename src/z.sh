2ff10cf312261bbd1b45957404582f2c990ed583
sigoden <sigoden@gmail.com>
2023-11-30 21:00:35 +0800

    feat: remove bash-only jobs/trap/ulimit/z commands (#27)

--- src/z.sh
+++
@@ -1,9 +0,0 @@
-_patch_help() {
-    _patch_help_run_man $@
-}
-
-_patch_table() {
-    _patch_table_add_metadata combine-shorts | \
-    _patch_table_edit_commands ';;' | \
-    _patch_table_edit_arguments ';;'
-}
