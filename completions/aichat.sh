5edc664c2ff2e046a96e516fd864e8d7c0d0b6c0
sigoden <sigoden@gmail.com>
2024-03-01 14:37:11 +0000

    aichat: sync updates

--- completions/aichat.sh
+++ completions/aichat.sh
@@ -4,6 +4,8 @@
 # @option -m --model[`_choice_model`]        Choose a LLM model
 # @option -r --role[`_choice_role`]          Choose a role
 # @option -s --session[`_choice_session`]    Create or reuse a session
+# @flag -e --execute                         Execute commands using natural language
+# @flag -c --code                            Generate only code
 # @option -f --file*                         Attach files to the message to be sent
 # @flag -H --no-highlight                    Disable syntax highlighting
 # @flag -S --no-stream                       No stream output
