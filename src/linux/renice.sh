63d9fa9a8d252a941f5b160e725113e38e1f2032
sigoden <sigoden@gmail.com>
2024-08-21 09:13:45 +0800

    update

--- src/linux/renice.sh
+++
@@ -1,8 +0,0 @@
-_patch_table() {
-    _patch_table_edit_options \
-        '--pgrp(<value>)' \
-        '--pid;[`_module_os_pid`]' \
-        '--user;[`_module_os_user`]' \
-    | \
-    _patch_table_edit_arguments ';;' '<priority>'
-}
